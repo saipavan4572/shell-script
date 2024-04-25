25a50ca2227da6ea7c44f367cf1a179dd6e50586
saipavan4572 <saipavan4572@gmail.com>
2024-04-25 19:21:11 +0530

    installation script

---
+++ 11-installation-2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access"
+else
+    echo "You are super user"
+fi
+
+dnf install mysql -y 
+
+echo "End of the script"
--- 11-installation.sh
+++ 11-installation.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 dnf install mysql -y 
+
+
+
+<<com
+if we run this script with only above command then we will get error as below
+
+[ ec2-user@ip-172-31-87-141 ~/shell-script ]$ sh 11-installation.sh
+Error: This command has to be run with superuser privileges (under the root user on most systems).
+
+com
