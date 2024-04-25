70cd68ae2b616d0119f81cb0fc0d7f343c2ab856
saipavan4572 <saipavan4572@gmail.com>
2024-04-25 17:01:58 +0530

    special-variables script

--- 09-special-variables.sh
+++ 09-special-variables.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
 echo "All variables: $@"
+echo "Number of variables passed: $#"
+
+
+
+
+
+<<com
+[ ec2-user@ip-172-31-87-141 ~/shell-script ]$ sh 09-special-variables.sh 10 20
+All variables: 10 20
+
+[ ec2-user@ip-172-31-87-141 ~/shell-script ]$ sh 09-special-variables.sh 10 20 harish girish 876abc 2#bc9u!zw
+-bash: !zw: event not found
+
+[ ec2-user@ip-172-31-87-141 ~/shell-script ]$ sh 09-special-variables.sh 10 20 harish girish 876abc 2#bc9u*x
+All variables: 10 20 harish girish 876abc 2#bc9u*x
+
+
+com
