d1863b539f303c51d4cef94e0d945141a2ade00f
saipavan4572 <saipavan4572@gmail.com>
2024-04-25 18:12:07 +0530

    conditions script

--- 09-special-variables.sh
+++ 09-special-variables.sh
@@ -84,4 +84,24 @@ sleep 60 ---> sleep for 60 sec and we can check which script is running with pro
 
 ec2-user    2495    1861  0 11:44 pts/1    00:00:00 sh 09-special-variables.sh 10 20 harish girish 876abc 2#bc9u*x
 
+9) $!
+[ ec2-user@ip-172-31-87-141 ~/shell-script ]$ sh 09-special-variables.sh 10 20 harish girish 876abc 2#bc9u*x
+All variables: 10 20 harish girish 876abc 2#bc9u*x
+Number of variables passed: 6
+Script Name: 09-special-variables.sh
+Current working directory: /home/ec2-user/shell-script
+Home directory of current user: /home/ec2-user
+Which user is running this script: ec2-user
+Hostname: ip-172-31-87-141.ec2.internal
+Process ID of the current shell script: 2580
+Process ID of last background command: 2581
+
+once we ran the above command we can check the PID:2581 process details in another terminal whether it is running or not
+
+[ ec2-user@ip-172-31-87-141 ~ ]$ ps -ef|grep sleep
+ec2-user    2581       1  0 11:53 pts/1    00:00:00 sleep 60
+ec2-user    2586    2501  0 11:53 pts/0    00:00:00 grep --color=auto sleep
+
+
+
 com
---
+++ 10-conditions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+Num1=$1
+Num2=$2
+
+if [ $Num1 -gt $Num2 ]
+    echo "Number $Num1 is greater than $Num2"
+else
+    echo "Number $Num1 is less than $Num2"
+fi
