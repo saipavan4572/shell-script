56aea6dfb9c500ed52354c491ca2a1e9e37bd320
saipavan4572 <saipavan4572@gmail.com>
2024-04-25 17:10:35 +0530

    special-variables script

--- 09-special-variables.sh
+++ 09-special-variables.sh
@@ -7,6 +7,7 @@ echo "Current working directory: $PWD"
 echo "Home directory of current user: $HOME"
 echo "Which user is running this script: $USER"
 echo "Hostname: $HOSTNAME"
+echo "Process ID of the current shell script: $$"
 
 
 
@@ -57,7 +58,16 @@ Home directory of current user: /home/ec2-user
 Which user is running this script: ec2-user
 
 7) $HOSTNAME
+[ ec2-user@ip-172-31-87-141 ~/shell-script ]$ sh 09-special-variables.sh 10 20 harish girish 876abc 2#bc9u*x
+All variables: 10 20 harish girish 876abc 2#bc9u*x
+Number of variables passed: 6
+Script Name: 09-special-variables.sh
+Current working directory: /home/ec2-user/shell-script
+Home directory of current user: /home/ec2-user
+Which user is running this script: ec2-user
+Hostname: ip-172-31-87-141.ec2.internal
 
+8) $$
 
 
 com
