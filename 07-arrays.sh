562cfcdda83fbdfc40a88627ad6d114f3dd72661
saipavan4572 <saipavan4572@gmail.com>
2024-04-25 15:42:38 +0530

    arrays script

---
+++ 07-arrays.sh
@@ -0,0 +1,13 @@
+#!/bin/bahs
+
+movies=("Pushpa" "RRR" "Salar")
+
+#size of the above array is 3
+# index are 0,1,2
+#list always start with 0
+
+echo "First movie is: ${movies[0]}"
+echo "Second movie is: ${movies[1]}"
+
+#to print all the contents/values in the array
+echo "First movie is: ${movies[@]}"
