8ea29ba973ab86f2d406aff8ee35938f91ff184d
saipavan4572 <saipavan4572@gmail.com>
2024-04-25 15:16:20 +0530

    variable-read script

---
+++ 05-variable-read.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Please enter username:"
+read USERNAME       ## Here USERNAME is a variable
+echo "Please enter password:"
+read PASSWORD       ## Here PASSWORD is a variable
+
+echo "Username is : $USERNAME , Password is: $PASSWORD"
