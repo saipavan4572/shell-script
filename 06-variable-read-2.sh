f471b69f2ad518143d008e99b07a231c2e1f9436
saipavan4572 <saipavan4572@gmail.com>
2024-04-25 15:27:16 +0530

    variable-read script

---
+++ 06-variable-read-2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Please enter username:" read -s USERNAME
+# with this read command actual entered username value will not be visible on the console
+
+echo "Please enter password:" read -s PASSWORD
+# with this read command actual entered password value will not be visible on the console
+
+echo "Username is : $USERNAME , Password is: $PASSWORD"
