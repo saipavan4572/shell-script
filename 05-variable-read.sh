9d0da7627e9836ab8b6ecbbdd66eb2afef353a1f
saipavan4572 <saipavan4572@gmail.com>
2024-04-25 18:27:22 +0530

    variable-read script

--- 05-variable-read.sh
+++ 05-variable-read.sh
@@ -9,3 +9,8 @@ read PASSWORD       ## Here PASSWORD is a variable
 # with this read command actual entered password value is visible on the console
 
 echo "Username is : $USERNAME , Password is: $PASSWORD"
+
+#instead of reading value in next line we can read in the same line after the string/text
+read -P "Enter a value: " VALUE
+
+echo You entered a value : $VALUE 
