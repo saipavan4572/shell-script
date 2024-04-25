61042ebcd5e4776f85689a381535935778b963c5
saipavan4572 <saipavan4572@gmail.com>
2024-04-25 16:38:00 +0530

    datatypes script

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -17,3 +17,20 @@ quot=$(($Num1 / $Num2))
       echo "The quotient is $quot"
 
 ##example run: sh 08-data-types.sh 40 20
+<<com
+
+echo "Scenario:1 number and string"
+[ ec2-user@ip-172-31-87-141 ~/shell-script ]$ bash 08-data-types.sh 60 hello
+Sum of 60 and hello is: 60
+The difference is 60
+The product is 0
+08-data-types.sh: line 16: 60 / hello: division by 0 (error token is "hello")
+The quotient is
+
+echo "Scenario:2 String and number"
+[ ec2-user@ip-172-31-87-141 ~/shell-script ]$ bash 08-data-types.sh hai 20
+Sum of hai and 20 is: 20
+The difference is -20
+The product is 0
+The quotient is 0
+com
