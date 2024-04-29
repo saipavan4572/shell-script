00016b7d4a3d04069734b11e78eb1cadd77551fb
saipavan4572 <saipavan4572@gmail.com>
2024-04-29 20:31:08 +0530

    calling script inside another script

--- 19-current-script-source.sh
+++ 19-current-script-source.sh
@@ -6,7 +6,7 @@ COURSE="Devops from current script"
 echo "Before calling other script : $COURSE"
 echo "Process id of current script: $$"
 
-source ./other-script.sh
+source ./18-other-script.sh
 
 echo "After calling other script : $COURSE"
 echo "Process id of current script: $$"
