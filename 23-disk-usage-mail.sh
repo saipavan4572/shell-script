87151a4613b9a7d50647e1c4d63c2acc4b0a066e
saipavan4572 <saipavan4572@gmail.com>
2024-05-01 23:34:38 +0530

    disk-usage-mail script

---
+++ 23-disk-usage-mail.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep xfs)
+DISK_THRESHOLD=10
+MESSAGE=""
+
+while IFS= read -r line
+do
+    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    FOLDER=$(echo $line | awk -F " " '{print $NF}')
+
+    if [ $USAGE -ge $DISK_THRESHOLD ]
+    then
+        ##MESSAGE="$FOLDER is more than $DISK_THRESHOLD, Current Usage: $USAGE" --> override previous value
+        MESSAGE+="$FOLDER is more than $DISK_THRESHOLD, Current Usage: $USAGE \n"
+    fi
+
+done <<< $DISK_USAGE
+
+echo "Message : $MESSAGE"
