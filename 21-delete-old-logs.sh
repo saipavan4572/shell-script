717515baf8e8876652f69459a04156de3e42c432
saipavan4572 <saipavan4572@gmail.com>
2024-05-01 22:15:31 +0530

    disk-usage script

--- 21-delete-old-logs.sh
+++ 21-delete-old-logs.sh
@@ -73,6 +73,26 @@ crontab: installing new crontab
 [ ec2-user@ip-172-31-25-61 ~/shell-script ]$ crontab -l
 */2 * * * * sh /home/ec2-user/shell-script/21-delete-old-logs.sh
 
+After running the above crontab below are the sample cron logs to delete the files
+
+[ root@ip-172-31-25-61 /var/log ]# tail -f cron
+May  1 15:52:01 ip-172-31-25-61 CROND[3628]: (ec2-user) CMDOUT (Files to delete : )
+May  1 15:52:01 ip-172-31-25-61 CROND[3628]: (ec2-user) CMDOUT (Deleting files: )
+May  1 15:52:01 ip-172-31-25-61 CROND[3628]: (ec2-user) CMDEND (sh /home/ec2-user/shell-script/21-delete-old-logs.sh)
+May  1 15:54:01 ip-172-31-25-61 CROND[3652]: (ec2-user) CMD (sh /home/ec2-user/shell-script/21-delete-old-logs.sh)
+May  1 15:54:01 ip-172-31-25-61 CROND[3651]: (ec2-user) CMDOUT (#033[32m Soruce Directory exists #033[0m)
+May  1 15:54:01 ip-172-31-25-61 CROND[3651]: (ec2-user) CMDOUT (Files to delete : /tmp/app-logs/test2.log)
+May  1 15:54:01 ip-172-31-25-61 CROND[3651]: (ec2-user) CMDOUT (/tmp/app-logs/test1.log)
+May  1 15:54:01 ip-172-31-25-61 CROND[3651]: (ec2-user) CMDOUT (Deleting files: /tmp/app-logs/test2.log)
+May  1 15:54:01 ip-172-31-25-61 CROND[3651]: (ec2-user) CMDOUT (Deleting files: /tmp/app-logs/test1.log)
+May  1 15:54:01 ip-172-31-25-61 CROND[3651]: (ec2-user) CMDEND (sh /home/ec2-user/shell-script/21-delete-old-logs.sh)
+May  1 15:56:01 ip-172-31-25-61 CROND[3774]: (ec2-user) CMD (sh /home/ec2-user/shell-script/21-delete-old-logs.sh)
+May  1 15:56:01 ip-172-31-25-61 CROND[3773]: (ec2-user) CMDOUT (#033[32m Soruce Directory exists #033[0m)
+May  1 15:56:01 ip-172-31-25-61 CROND[3773]: (ec2-user) CMDOUT (Files to delete : /tmp/app-logs/test4.log)
+May  1 15:56:01 ip-172-31-25-61 CROND[3773]: (ec2-user) CMDOUT (/tmp/app-logs/test3.log)
+May  1 15:56:01 ip-172-31-25-61 CROND[3773]: (ec2-user) CMDOUT (Deleting files: /tmp/app-logs/test4.log)
+May  1 15:56:01 ip-172-31-25-61 CROND[3773]: (ec2-user) CMDOUT (Deleting files: /tmp/app-logs/test3.log)
+May  1 15:56:01 ip-172-31-25-61 CROND[3773]: (ec2-user) CMDEND (sh /home/ec2-user/shell-script/21-delete-old-logs.sh)
 
 com
 
---
+++ 22-disk-usage.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep xfs)
+DISK_THRESHOLD=10
+
+while IFS = read -r line
+do
+    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    FOLDER=$(echo $line | awk -F " " '{print $6F}')
+
+    if [ $USAGE -ge $DISK_THRESHOLD ]
+    then
+        echo "$FOLDER is more than $DISH_THRESHOLD, Current Usage: $USAGE"
+    fi
+
+done <<< $DISK_USAGE
