48aa39a993f221d6e6d7188f535c44379991cdaf
saipavan4572 <saipavan4572@gmail.com>
2024-05-01 18:04:36 +0530

    set script

--- 20-set.sh
+++ 20-set.sh
@@ -2,7 +2,12 @@
 
 set -e
 
-USERID=$(id -u)
+failure(){
+    echo "Failed at :$1  :: $2"
+}
+trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
+
+USERID=$(id -u) #ERR ---> Error signal
 
 if [ $USERID -ne 0 ]
 then
@@ -16,3 +21,18 @@ dnf install mysqlsldkf -y
 dnf install git -y
 
 echo "End of the script."
+
+<<com
+
+set -e ---> shell script checks every instruction/command manually and stop/exit if any error occurs
+This method is uesful in while working on the Crontab cron jobs.
+
+But there is no much performance diffrence between this "set -e" command and any manully created method used for validation(ex: VALIDATE)
+
+[ ec2-user@ip-172-31-25-61 ~/shell-script ]$ sudo sh 20-set.sh
+You are super user.
+Last metadata expiration check: 0:00:38 ago on Wed May  1 12:15:35 2024.
+No match for argument: mysqlsldkf
+Error: Unable to find a match: mysqlsldkf
+
+com
