6f6d8a49d637df3866ed620a35509bb3ad2abcbb
saipavan4572 <saipavan4572@gmail.com>
2024-05-01 20:02:40 +0530

    old logs delete script

--- 21-delete-old-logs.sh
+++ 21-delete-old-logs.sh
@@ -15,6 +15,10 @@ else
     exit 1
 fi
 
+FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +10)
+
+echo "Files to delete : " $FILES
+
 <<com
 case-1 if the directory exists
 [ ec2-user@ip-172-31-25-61 ~/shell-script ]$ sh 21-delete-old-logs.sh
@@ -24,7 +28,20 @@ case-1 if the directory exists
 [ ec2-user@ip-172-31-25-61 ~/shell-script ]$ sh 21-delete-old-logs.sh
  Please make sure /tmp/app-logsabc exists!!!
 
+find command to fetch/get only the specific extension files(ex: log files)
+[ ec2-user@ip-172-31-25-61 /tmp/app-logs ]$ find . -name "*.log"
+./common-test.log
+./test3.log
+./test2.log
+./test1.log
 
+to fetch the log files which are older than from today is:
+find . -type f -name "*.log" -mtime +10
+---->> -mtime - modified time
+[ ec2-user@ip-172-31-25-61 /tmp/app-logs ]$ find . -name "*.log" -mtime +10
+./common-test.log
+./test2.log
+./test1.log
 
 com
 
