00585e741d3232b0f19dad7f630b1c0ee796045c
saipavan4572 <saipavan4572@gmail.com>
2024-05-01 22:22:14 +0530

    disk-usage script

--- 22-disk-usage.sh
+++ 22-disk-usage.sh
@@ -6,7 +6,7 @@ DISK_THRESHOLD=10
 while IFS= read -r line
 do
     USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
-    FOLDER=$(echo $line | awk -F " " '{print $6F}')
+    FOLDER=$(echo $line | awk -F " " '{print $nF}')
 
     if [ $USAGE -ge $DISK_THRESHOLD ]
     then
