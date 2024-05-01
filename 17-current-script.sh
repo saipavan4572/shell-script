c6c47c562f033228fdeccb9c26cfb3e50ff7b41c
saipavan4572 <saipavan4572@gmail.com>
2024-05-01 17:42:37 +0530

    set script

--- 17-current-script.sh
+++ 17-current-script.sh
@@ -10,3 +10,14 @@ echo "Process id of current script: $$"
 echo "After calling other script : $COURSE"
 echo "Process id of current script: $$"
 
+<<com
+
+$ sh 17-current-script.sh
+Before calling other script : Devops from current script
+Process id of current script: 1449
+variable value from other script : Devops from other script
+Process id of current script: 1450
+After calling other script : Devops from current script
+Process id of current script: 1449
+
+com
--- 19-current-script-source.sh
+++ 19-current-script-source.sh
@@ -10,3 +10,16 @@ source ./18-other-script.sh
 
 echo "After calling other script : $COURSE"
 echo "Process id of current script: $$"
+
+<<com
+
+$ sh 19-current-script-source.sh
+Before calling other script : Devops from current script
+Process id of current script: 1503
+variable value from other script : Devops from other script
+Process id of current script: 1503
+After calling other script : Devops from other script
+Process id of current script: 1503
+
+
+com
