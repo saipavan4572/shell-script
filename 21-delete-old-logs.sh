97bb19811afad5952a0c1623e7bf285cbc2a01ae
saipavan4572 <saipavan4572@gmail.com>
2024-05-01 20:47:12 +0530

    old logs delete script

--- 21-delete-old-logs.sh
+++ 21-delete-old-logs.sh
@@ -19,6 +19,12 @@ FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +10)
 
 echo "Files to delete : $FILES"
 
+while IFS= read -r line
+do
+    echo "Deleting files: $line"
+done <<<$FILES
+
+
 <<com
 case-1 if the directory exists
 [ ec2-user@ip-172-31-25-61 ~/shell-script ]$ sh 21-delete-old-logs.sh
@@ -35,13 +41,24 @@ find command to fetch/get only the specific extension files(ex: log files)
 ./test2.log
 ./test1.log
 
-to fetch the log files which are older than from today is:
+to fetch the files with .log extension which are older than from today is:
 find . -type f -name "*.log" -mtime +10
 ---->> -mtime - modified time
 [ ec2-user@ip-172-31-25-61 /tmp/app-logs ]$ find . -name "*.log" -mtime +10
+./mysql-test.log
 ./common-test.log
 ./test2.log
 ./test1.log
+
+---> ./mysql-test.log is a directory
+
+to fetch the .log files which are older than from today is:
+[ ec2-user@ip-172-31-25-61 /tmp/app-logs ]$ find . -name "*.log" -type f -mtime +10
+./common-test.log
+./test2.log
+./test1.log
+
+IFS - Internal Field Separator
 
 com
 
